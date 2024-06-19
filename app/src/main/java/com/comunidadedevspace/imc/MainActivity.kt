b7d650a5f365e6dc40b869404ce3a991bd0be774
Robson <robson.jso@hotmail.com>
2024-06-19 06:34:11 -0300

    campos vazios

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,6 +3,7 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +16,31 @@ class MainActivity : AppCompatActivity() {
 
         val btnCalcula = findViewById<Button>(R.id.btn_calc)
 
-        btnCalcula.setOnClickListener{
-            val peso: Float = edtPeso.text.toString().toFloat()
-            val altura: Float = edtAltura.text.toString().toFloat()
+        btnCalcula.setOnClickListener {
 
-            val alturaQ2 = altura * altura
-            val resultado = peso / alturaQ2
-            println("Robson ação do botao" + resultado)
+            val pesoStr: String = edtPeso.text.toString()
+            val alturaStr: String = edtAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                //mostrar mensagem para usuario
+                Snackbar
+                    .make(
+                        edtPeso,
+                        "Preencha todos os campos",
+                        Snackbar.LENGTH_LONG
+                    )
+                    .show()
+
+            } else {
+                val peso = edtPeso.text.toString().toFloat()
+                val altura = edtAltura.text.toString().toFloat()
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+
+                println("Robson ação do botao" + resultado)
+            }
 
         }
     }
